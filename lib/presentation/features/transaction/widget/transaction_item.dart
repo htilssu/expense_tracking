465d632c2c7078f472900eed69b6d892be775af0
Trung Hieu <81826098+htilssu@users.noreply.github.com>
2025-03-03 20:17:12 +0700

    Add phân loại giao dịch trong lịch sử
    🐛 (presentation): fix transaction item to show category name and type

--- lib/presentation/features/transaction/widget/transaction_item.dart
+++ lib/presentation/features/transaction/widget/transaction_item.dart
@@ -1,9 +1,11 @@
 import 'package:expense_tracking/constants/app_theme.dart';
 import 'package:expense_tracking/constants/text_constant.dart';
+import 'package:expense_tracking/domain/entity/category.dart';
 import 'package:expense_tracking/presentation/bloc/category/category_bloc.dart';
 import 'package:expense_tracking/utils/currency_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../../../domain/entity/transaction.dart';
 import '../../../../utils/date_format.dart';
@@ -19,75 +21,92 @@ class TransactionItem extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 8),
       child: Padding(
         padding: EdgeInsets.all(2),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: AppTheme.placeholderColor.withAlpha(20),
-              ),
-              child: Center(
-                child: Text(
-                  //TODO: get category icon
-                  "😊",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: TextSize.large),
-                ),
-              ),
-            ),
-            SizedBox(width: 8),
-            Expanded(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: BlocBuilder<CategoryBloc, CategoryState>(
+          builder: (context, state) {
+            Category? category;
+            if (state is CategoryLoaded) {
+              category = state.categories
+                  .firstWhere((element) => element.id == _transaction.category);
+            }
+
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                BlocBuilder<CategoryBloc, CategoryState>(
-                  builder: (context, state) {
-                    if (state is CategoryLoaded) {
-                      return Text(
-                        state.categories
-                            .firstWhere((element) =>
-                                element.id == _transaction.category)
-                            .name,
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppTheme.placeholderColor.withAlpha(20),
+                  ),
+                  child: Center(
+                    child: Text(
+                      //TODO: get category icon
+                      "😊",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: TextSize.large),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (state is CategoryLoaded)
+                      Text(
+                        category!.name,
                         style: TextStyle(
                             fontSize: TextSize.medium,
                             fontWeight: FontWeight.bold),
-                      );
-                    } else {
-                      return Text("Loading...");
-                    }
-                  },
-                ),
-                Text(
-                  _transaction.note,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                      fontSize: TextSize.medium,
-                      fontWeight: FontWeight.normal,
-                      color: AppTheme.hintColor.withAlpha(120)),
+                      )
+                    else
+                      Shimmer.fromColors(
+                        baseColor: Colors.grey[300]!,
+                        highlightColor: Colors.grey[100]!,
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.grey[300],
+                          ),
+                        ),
+                      ),
+                    Text(
+                      _transaction.note,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: TextSize.medium,
+                          fontWeight: FontWeight.normal,
+                          color: AppTheme.hintColor.withAlpha(120)),
+                    )
+                  ],
+                )),
+                SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "${category!.type == "income" ? "+" : "-"} ${CurrencyFormatter.formatCurrency(_transaction.value)}",
+                      style: TextStyle(
+                        fontSize: TextSize.medium + 4,
+                        color: category.type == "income"
+                            ? Colors.green
+                            : Colors.red[300],
+                      ),
+                    ),
+                    Text(
+                      Date.format(_transaction.createdAt),
+                      style:
+                          TextStyle(color: AppTheme.hintColor.withAlpha(120)),
+                    )
+                  ],
                 )
               ],
-            )),
-            SizedBox(width: 16),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  CurrencyFormatter.formatCurrency(_transaction.value),
-                  style: TextStyle(
-                    fontSize: TextSize.medium + 4,
-                  ),
-                ),
-                Text(
-                  Date.format(_transaction.createdAt),
-                  style: TextStyle(color: AppTheme.hintColor.withAlpha(120)),
-                )
-              ],
-            )
-          ],
+            );
+          },
         ),
       ),
     );
