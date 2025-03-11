6bef340e4e5cea9db23e10a056f8437997388e20
htilssu <tolashuu@gmail.com>
2025-03-11 13:37:30 +0700

    🐛 (auth.dart): remove authStream to prevent memory leaks 🐛 (auth.dart): add try-catch block to handle exceptions

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -8,24 +8,13 @@ class Auth {
   ///- Get the current user's id
   ///- Throw [AuthenticateException] if the user is not logged in
   static String uid() {
-    var usr = FirebaseAuth.instance.currentUser;
-    if (usr == null) throw AuthenticateException('User is not logged in');
+    try {
+      var usr = FirebaseAuth.instance.currentUser;
+      if (usr == null) throw AuthenticateException('User is not logged in');
 
-    return usr.uid;
-  }
-
-  static final authStream = StreamController<User?>();
-
-  static Stream<User?> getStream() {
-    authStream.add(FirebaseAuth.instance.currentUser);
-    return authStream.stream;
-  }
-
-  static void closeStream() {
-    authStream.close();
-  }
-
-  static void addStream(User? user) {
-    authStream.add(user);
+      return usr.uid;
+    } on Exception catch (e) {
+      throw AuthenticateException(e.toString());
+    }
   }
 }
