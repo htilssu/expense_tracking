1aa598982f2f7501920ce2f5857d8ccdee187a1c
htilssu <tolashuu@gmail.com>
2025-03-25 00:21:42 +0700

    🐛 (validator.dart): fix email validation regex to disallow leading special characters

--- lib/utils/validator.dart
+++ lib/utils/validator.dart
@@ -1,6 +1,6 @@
 class Validator {
   static bool isValidEmail(String email) {
-    return RegExp(r'^[a-zA-Z0-9.+]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$').hasMatch(email);
+    return RegExp(r'^(?=[a-zA-Z0-9])[a-zA-Z0-9.+]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$').hasMatch(email);
   }
   static bool isValidPassword(String password){
     return RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$!%*?&]).{6,32}$').hasMatch(password);
